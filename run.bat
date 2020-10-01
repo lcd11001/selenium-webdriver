9ceadd293c0126bc3d520116bccc7cb4b324060f
lcd11001 <lcd11001@gmail.com>
2020-10-01 11:03:15 +0700

    + testing

---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo OFF
+
+set CURR_DIR=%~dp0
+
+set CHROME_DRIVER_DIR=%CURR_DIR%node_modules\chromedriver\lib\chromedriver
+set FIREFOX_DRIVER_DIR=%CURR_DIR%node_modules\geckodriver
+
+set PATH=%PATH%;%CHROME_DRIVER_DIR%;%FIREFOX_DRIVER_DIR%
+
+call node %CURR_DIR%source\library.js
