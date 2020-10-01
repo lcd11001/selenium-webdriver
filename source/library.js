8d11d7d4bc2a3f8d043ffbf0b324fcaeafdc6e5c
lcd11001 <lcd11001@gmail.com>
2020-10-01 17:19:38 +0700

    * found menu items

--- source/library.js
+++ source/library.js
@@ -3,27 +3,40 @@ var webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
 
-        
 
-        var driver = new webdriver.Builder().forBrowser('chrome').build();
-        
-        driver.get('https://library-app.firebaseapp.com/');
-        
-        driver.findElement(By.className('ember-text-field'))
-        .catch(err => {
-            console.log('1', err)
-        })
 
-        driver.findElement(By.className('btn-primary'))
-        .catch(err => {
-            console.log('2', err)
-        })
+var driver = new webdriver.Builder().forBrowser('chrome').build();
 
-        driver.findElement(By.css('alert-success'))
-        .catch(err => {
-            console.log('3', err)
-        })
-        
-        
-        driver.sleep(10000);
-        driver.quit();
+driver.get('https://library-app.firebaseapp.com/');
+
+driver.findElement(By.className('ember-text-field'))
+    .then(ele => {
+        console.log('found input', ele)
+    })
+    .catch(err => {
+        console.log('1', err)
+    })
+
+driver.findElement(By.className('btn-primary'))
+    .then(ele => {
+        console.log('found button', ele)
+    })
+    .catch(err => {
+        console.log('2', err)
+    })
+
+// driver.findElement(By.css('alert-success'))
+// .catch(err => {
+//     console.log('3', err)
+// })
+
+driver.findElements(By.css('ul li'))
+    .then(arr_ele => {
+        console.log('found array', arr_ele)
+    })
+    .catch(err => {
+        console.log('4', err)
+    })
+
+driver.sleep(10000);
+driver.quit();
