b1ac07f18faa327cdfa7f8168ee12197248a9e82
lcd11001 <lcd11001@gmail.com>
2020-10-01 17:02:59 +0700

    * stable version

--- source/library.js
+++ source/library.js
@@ -1,7 +1,29 @@
+
 var webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
 
-var driver = new webdriver.Builder().forBrowser('chrome').build();
+        
 
-driver.get('https://library-app.firebaseapp.com/')
+        var driver = new webdriver.Builder().forBrowser('chrome').build();
+        
+        driver.get('https://library-app.firebaseapp.com/');
+        
+        driver.findElement(By.className('ember-text-field'))
+        .catch(err => {
+            console.log('1', err)
+        })
+
+        driver.findElement(By.className('btn-primary'))
+        .catch(err => {
+            console.log('2', err)
+        })
+
+        driver.findElement(By.css('alert-success'))
+        .catch(err => {
+            console.log('3', err)
+        })
+        
+        
+        driver.sleep(10000);
+        driver.quit();
